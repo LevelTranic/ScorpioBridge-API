7ddb6143c7befe88c3d7191569d5d88187a01f59
404Setup <153366651+404Setup@users.noreply.github.com>
2024-09-26 22:20:49 +0800

    Deprecating shared schedulers

--- src/main/kotlin/one/tranic/scorpio/api/util/Scheduler.kt
+++ src/main/kotlin/one/tranic/scorpio/api/util/Scheduler.kt
@@ -4,6 +4,7 @@ import com.google.common.util.concurrent.ThreadFactoryBuilder
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
 
+@Deprecated("You should not use this method, each plugin should manually maintain its own asynchronous thread pool instead of using the shared thread pool provided by the API.")
 object Scheduler {
     private val dispatcher = Executors.newScheduledThreadPool(
         8, ThreadFactoryBuilder()
@@ -12,10 +13,12 @@ object Scheduler {
             .build()
     )
 
+    @Deprecated("You should not use this method, each plugin should manually maintain its own asynchronous thread pool instead of using the shared thread pool provided by the API.")
     fun put(block: Runnable): Future<*> {
         return dispatcher.submit(block)
     }
 
+    @Deprecated("You should not use this method, each plugin should manually maintain its own asynchronous thread pool instead of using the shared thread pool provided by the API.")
     fun shutdown() {
         dispatcher.shutdown()
     }
