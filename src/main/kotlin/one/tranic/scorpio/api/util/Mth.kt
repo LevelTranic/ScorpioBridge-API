ced177e46d77ac47ec940081e91f3a52c73768dc
404Setup <153366651+404Setup@users.noreply.github.com>
2024-09-26 22:27:07 +0800

    full javadoc

--- src/main/kotlin/one/tranic/scorpio/api/util/Mth.kt
+++ src/main/kotlin/one/tranic/scorpio/api/util/Mth.kt
@@ -1,6 +1,17 @@
 package one.tranic.scorpio.api.util
 
+/**
+ * Utility object for mathematical and numerical operations in the Scorpio API.
+ * This object provides methods to validate number inputs.
+ */
 object Mth {
+
+    /**
+     * Checks if the given input string represents a valid number within the range of -1000 to 1000.
+     *
+     * @param input the string input to be validated
+     * @return true if the input is a valid integer within the specified range, false otherwise
+     */
     fun isValidNumber(input: String): Boolean {
         return input.toIntOrNull()?.let { it in -1000..1000 } ?: false
     }
--- src/main/kotlin/one/tranic/scorpio/api/util/Runtime.kt
+++ src/main/kotlin/one/tranic/scorpio/api/util/Runtime.kt
@@ -1,8 +1,20 @@
 package one.tranic.scorpio.api.util
 
+/**
+ * Utility object for runtime-related functions in the Scorpio API.
+ * This object provides methods to retrieve information about the Java runtime environment.
+ */
 object Runtime {
     private var javaVersion: Int? = null
 
+    /**
+     * Retrieves the major version of the Java Runtime Environment.
+     *
+     * The version is returned as an integer. For example, for Java 8, it returns 8;
+     * for Java 11, it returns 11.
+     *
+     * @return the major Java version as an Int
+     */
     fun getJavaVersion(): Int {
         if (javaVersion != null) {
             return javaVersion!!
